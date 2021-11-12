33fdc58f5f94040e49bd4df49b31c0f07c68648c
Julien Zamor <julien@cit.io>
2021-11-12 18:12:31 +0100

    relative font import

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -386,7 +386,7 @@ module.exports = function (webpackEnv) {
               loader: require.resolve('url-loader'),
               options: {
                 limit: imageInlineSizeLimit,
-                name: 'static/media/[name].[hash:8].[ext]',
+                name: '../../static/media/[name].[hash:8].[ext]',
               },
             },
             // Process application JS with Babel.
@@ -547,7 +547,7 @@ module.exports = function (webpackEnv) {
               // by webpacks internal loaders.
               exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
               options: {
-                name: 'static/media/[name].[hash:8].[ext]',
+                name: '../../static/media/[name].[hash:8].[ext]',
               },
             },
             // ** STOP ** Are you adding a new loader?
